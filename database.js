5d4af3076f2b4b15bf3937633afe26e751800ca1
evankozliner <evankozliner@gmail.com>
2015-12-30 17:50:26 -0500

    fixed a quick syntax bug I overlooked

--- database.js
+++ database.js
@@ -1,21 +1,25 @@
 var AWS = require('aws-sdk');
+var app = require('./app');
 // set aws region
 AWS.config.region = 'us-west-2';
 
-if (app.get('env') === 'development') {
-	// Local dynamo db configuration
-	AWS.config.update({
-	  endpoint: "http://localhost:8000"
-	});
-} else {
-	// Production configuration
-	AWS.config.update({endpoint: "https://dynamodb.us-west-2.amazonaws.com"});
-}
+
+AWS.config.update({endpoint: "http://localhost:8000"});
+
+//if (app.get('env') === 'development') {
+//	// Local dynamo db configuration
+//	AWS.config.update({
+//	  endpoint: "http://localhost:8000"
+//	});
+//} else {
+//	// Production configuration
+//	AWS.config.update({endpoint: "https://dynamodb.us-west-2.amazonaws.com"});
+//}
 
 var db = new AWS.DynamoDB();
 
 exports.getTables = function() {
-	db.listTables('/', function(err, data) {
+	db.listTables(function(err, data) {
 		console.log(data)
 		return data
 	});
